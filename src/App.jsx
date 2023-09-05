5dede0e78ec4c0711b2af207ed7ee4f59c95c4e1
samarth goyal <samarthagrawal1804@gmail.com>
2023-09-05 22:18:56 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-import { useState } from 'react'
-import Task from './components/Task'
+import { useState } from "react";
+import Task from "./components/Task";
 function App() {
- 
-
   return (
-   <>
-   <div className="w-full min-h-screen bg-gray-100 box-border">
-    
-      <header className='w-full h-10 bg-blue-500 text-white text-lg flex justify-center items-center'>Get Ready to complete your daily goals.</header>
-   
-    <div className="w-full max-w-3xl h-auto bg-red-200 mx-auto px-20 my-10 overflow-hidden">
-      <h1 className='text-3xl font-bold text-center my-10 '>Daily Goals</h1>
-      <div className="bg-violet-400">
-        <form className='flex flex-col '>
-          <input type="text" className='border-2 border-solid border-gray-200 my-5 py-2 px-5'/>
-          <textarea className='border-2 border-solid border-gray-200'></textarea>
-          <button type='submit'>Add Task</button>
-        </form>
-      </div>
-    <Task />
+    <>
+      <div className="w-full min-h-screen bg-gray-200 box-border">
+        <header className="w-full h-10 bg-blue-500 text-white text-lg flex justify-center items-center">
+          Get Ready to complete your daily goals.
+        </header>
 
-    </div>
-   </div>
-   </>
-  )
+        <div className="w-full max-w-2xl h-auto mx-auto  my-5 overflow-hidden rounded-3xl">
+          <h1 className="text-3xl font-bold text-center my-4 ">Daily Goals</h1>
+          <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-violet-500 to-90% p-5 rounded-lg mx-4">
+            <form className="flex flex-col ">
+              <input
+                type="text"
+                placeholder="Title"
+                className="border-2 border-solid border-gray-200 my-5 py-2 px-5 rounded-md"
+              />
+              <textarea placeholder="Description" className="border-2 border-solid border-gray-200 rounded-md my-5"></textarea>
+              <button
+                type="submit"
+                className="bg-black text-white py-2 px-2 rounded-md "
+              >
+                Add Task
+              </button>
+            </form>
+          </div>
+        </div>
+        <Task />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
