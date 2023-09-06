e02b3262c7b50ec07f68ec9c7bf34a18f566bad5
samarth goyal <samarthagrawal1804@gmail.com>
2023-09-06 22:39:20 +0530

    Minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Task from "./components/Task";
 function App() {
   return (
     <>
-      <div className="w-full min-h-screen bg-gray-200 box-border">
+      <div className="w-full min-h-screen bg-slate-900 box-border">
         <header className="w-full h-10 bg-blue-500 text-white text-lg flex justify-center items-center">
           Get Ready to complete your daily goals.
         </header>
 
         <div className="w-full max-w-2xl h-auto mx-auto  my-5 overflow-hidden rounded-3xl">
           <h1 className="text-3xl font-bold text-center my-4 ">Daily Goals</h1>
-          <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-violet-500 to-90% p-5 rounded-lg mx-4">
+          <div className="bg-gradient-to-r from-indigo-500 from-20% via-sky-500 via-30% to-violet-500 to-90% p-5 rounded-lg mx-4">
             <form className="flex flex-col ">
               <input
                 type="text"
