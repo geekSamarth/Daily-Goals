57f60106be41773d2ece28cb42b61d1bd7222563
samarth goyal <samarthagrawal1804@gmail.com>
2023-09-09 22:55:27 +0530

    Adding complete logic of daily goals.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
 import { useState } from "react";
 import Task from "./components/Task";
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTasks([...tasks, { title, description }]);
+    setTitle("");
+    setDescription(""); 
+  };
+
+  const deleteTask = (index) => {
+    const filteredArr = tasks.filter((val,i)=>{
+      return i!=index
+    })
+    setTasks(filteredArr);
+  };
   return (
     <>
-      <div className="w-full min-h-screen bg-stone-950 box-border">
+      <div className="w-full min-h-screen bg-stone-950 box-border pb-10">
         <header className="w-full h-10 bg-blue-500 text-white text-lg flex justify-center items-center">
           Get Ready to complete your daily goals.
         </header>
 
-        <div className="w-full max-w-2xl h-auto mx-auto  overflow-hidden rounded-3xl">
-          <h1 className="text-4xl font-bold text-center my-4 text-green-400">Daily Goals</h1>
+        <div className="w-full max-w-2xl h-auto mx-auto  overflow-hidden">
+          <h1 className="text-4xl font-bold text-center my-4 text-green-400">
+            Daily Goals
+          </h1>
           <div className="bg-gradient-to-r from-indigo-500 from-20% via-sky-500 via-30% to-violet-500 to-90% p-3 rounded-lg mx-3">
-            <form className="flex flex-col ">
+            <form className="flex flex-col" onSubmit={submitHandler}>
               <input
                 type="text"
                 placeholder="Title"
                 className="border-2 border-solid border-gray-400 mb-5 py-2 px-5 rounded-md outline-none focus:border-2 focus:border-solid focus:border-black text-lg"
+                value={title}
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }}
               />
-              <textarea placeholder="Description" className="border-2 border-solid border-gray-400 rounded-md mb-5 py-2 px-5 outline-none focus:border-2 focus:border-solid focus:border-black h-12 text-lg"></textarea>
+              <textarea
+                placeholder="Description"
+                className="border-2 border-solid border-gray-400 rounded-md mb-5 py-2 px-5 outline-none focus:border-2 focus:border-solid focus:border-black h-12 text-lg"
+                value={description}
+                onChange={(e) => {
+                  setDescription(e.target.value);
+                }}
+              ></textarea>
               <button
                 type="submit"
-                className="bg-black text-white py-2 px-2 rounded-md text-2xl hover:bg-slate-900"
+                className="bg-black text-white py-2 px-2 rounded-md text-2xl hover:bg-slate-950"
               >
                 Add Task
               </button>
             </form>
           </div>
         </div>
-        <Task />
+        {tasks.map((item, index) => (
+          <Task key={index} title={item.title} description={item.description} deleteTask={deleteTask} index={index}/>
+        ))}
       </div>
-      
     </>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
-function Task() {
+function Task({ title, description, index, deleteTask }) {
   return (
-    <div>
-      <p>Title</p>
-      <span>Description</span>
+    <div className="w-full max-w-xl flex justify-between items-center mx-auto bg-violet-600 mt-7 rounded-lg px-3 py-2 bg-gradient-to-r from-indigo-500 from-20% via-sky-500 via-30% to-violet-500 to-90%">
+      <div className="px-2">
+        <p className="font-bold text-xl">{title}</p>
+        <span className="text-lg">{description}</span>
+      </div>
+      <button
+        className="bg-red-700 text-white px-6 py-2 text-lg font-bold rounded-md hover:bg-red-600"
+        onClick={() => deleteTask(index)}
+      >
+        Del
+      </button>
     </div>
   );
 }
